c18b046d131038f8ad490c601b066a32bd4f9dc0
connor <connormullett@gmail.com>
2020-10-15 00:11:06 -0400

    test refactoring with API mocking

--- src/test/test_lib.py
+++ src/test/test_lib.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest.mock import patch
 import os
 from dotenv import load_dotenv
 
@@ -9,40 +10,37 @@ from src.lib import request
 
 
 class TestEndpoints(unittest.TestCase):
-    def setUp(self):
+
+    @patch('src.lib.request.requests.get')
+    def setUp(self, mock_get):
         load_dotenv()
+        mock_get.return_value.status_code = 200
 
     def test_get_health_check(self):
         response, status_code = endpoints.get_health_check()
-        self.assertTrue(response)
         self.assertEqual(status_code, 200)
 
     def test_get_match_by_id(self):
         match_id = well_known_data.TEST_MATCH_ID
         response, status_code = endpoints.get_match_by_id(match_id)
-        self.assertTrue(response)
         self.assertEqual(status_code, 200)
 
     def test_get_players_by_rank(self):
         response, status_code = endpoints.get_players_by_rank()
-        self.assertTrue(response)
         self.assertEqual(status_code, 200)
 
     def test_get_player_by_account_id(self):
         account_id = well_known_data.TEST_ACCOUNT_ID
         response, status_code = endpoints.get_player_by_account_id(account_id)
-        self.assertTrue(response)
         self.assertEqual(status_code, 200)
 
     def test_get_hero_stats(self):
         response, status_code = endpoints.get_hero_stats()
-        self.assertTrue(response)
         self.assertEqual(status_code, 200)
 
     def test_get_constants(self):
         resource_name = well_known_data.TEST_CONSTANT_RESOURCE_NAME
         response, status_code = endpoints.get_constant_data(resource_name)
-        self.assertTrue(response)
         self.assertEqual(status_code, 200)
 
 
