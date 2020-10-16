794cfbef561fc52fb8ae6017be3e6984bb91c195
connor <connormullett@gmail.com>
2020-10-16 19:25:54 -0400

    added exceptions and configuration checking for required values

--- main.py
+++ main.py
@@ -2,7 +2,10 @@
 
 from dotenv import load_dotenv
 from src.lib.endpoints import get_health_check
+from src.config import check_config
 
 if __name__ == "__main__":
     load_dotenv()
-    get_health_check()
+    check_config()
+    response, status = get_health_check()
+    print(status)
--- src/constants.py
+++ src/constants.py
@@ -8,3 +8,11 @@ class API_URI_ENDPOINTS(Enum):
     PLAYERS_BY_ACCOUNT_ID = "players/%s"
     HERO_STATS = "heroStats"
     CONSTANTS = "constants/%s"
+
+
+ENVIRONMENT_VARIABLES_CONFIG = {
+    "OPEN_DOTA_API_BASE_URL": {
+        "required": True,
+        "description": "Base URL for hitting the API endpoints",
+    }
+}
