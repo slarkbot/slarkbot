c3ea144bc1f1eafc50d164ad6a68ca0b3c07f9d5
connor <connormullett@gmail.com>
2020-10-17 15:01:45 -0400

    database connection

--- src/bot/models/sessions.py
+++ src/bot/models/sessions.py
@@ -3,5 +3,5 @@ from sqlalchemy.orm import sessionmaker
 
 def create_session(engine):
     session = sessionmaker()
-    session.configure(engine)
-    return session
+    session.configure(bind=engine)
+    return session()
