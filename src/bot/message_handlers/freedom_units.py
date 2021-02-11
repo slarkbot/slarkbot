443167e557de352457c61d15988d1786f13d3f1a
Will Dixon <wdixon23192@gmail.com>
2021-02-11 13:24:12 -0500

    Update src/bot/message_handlers/freedom_units.py
    I have it on good authority this isnt gonna turn the output into a decimal trainwreck :P
    
    Co-authored-by: Connor Mullett <41083414+connormullett@users.noreply.github.com>

--- src/bot/message_handlers/freedom_units.py
+++ src/bot/message_handlers/freedom_units.py
@@ -15,7 +15,7 @@ def convert_to_freedom_units(update, context):
         match = match.group(0)
         print(match)
 
-        degrees_c = int(float(re.findall("\x2d?[0-9]*\x2e?[0-9]+", match)[0]))
+        degrees_c = float(re.findall("\x2d?[0-9]*\x2e?[0-9]+", match)[0])
         converted_units = (degrees_c * (9 / 5)) + 32
 
         output = f"{degrees_c} is {converted_units}f in freedom units"
