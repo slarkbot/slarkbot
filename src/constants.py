4d8629b15355d2e0121360f1d19dcddd5a9b3e80
connor <connormullett@gmail.com>
2021-02-14 00:59:05 -0500

    mmr is no longer available, use medal and tier

--- src/constants.py
+++ src/constants.py
@@ -132,3 +132,13 @@ GAME_MODE_MAP = {
     23: "Turbo",
     24: "Mutation",
 }
+
+RANKS = {
+    1: "Herald",
+    2: "Guardian",
+    3: "Crusader",
+    4: "Archon",
+    5: "Legend",
+    6: "Ancient",
+    7: "Divine"
+}
