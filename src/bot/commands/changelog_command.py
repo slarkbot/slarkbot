f295dd64d22d3237b73d76eae9f34e693bbf872d
connor <connormullett@gmail.com>
2021-02-23 18:18:11 -0500

    add changelog command

---
+++ src/bot/commands/changelog_command.py
@@ -0,0 +1,17 @@
+
+import os
+
+
+def get_latest_changelog(update, context):
+  version = os.getenv('SLARKBOT_VERSION')
+  root_dir = os.getenv('ROOT_DIR')
+
+  file_name = '%s-CHANGELOG.md' % version
+
+  archive_path = '%s/archive/%s' % root_dir, file_name
+
+  with open(archive_path, "r") as f:
+    content = f.read()
+
+  update.message.reply_text(content, parse_mode='MarkdownV2')
+
