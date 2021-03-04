edd31c5836f6eb8ead6a9c4721343c2a54384dd7
Casper Schuijt <casperschuijt@gmail.com>
2021-03-04 23:04:50 +0100

    xd

--- src/bot/message_handlers/youre_welcome.py
+++ src/bot/message_handlers/youre_welcome.py
@@ -1,7 +1,5 @@
 def say_youre_welcome(update, context):
     text = update.message.text
 
-    print("TEXT ", text)
-
-    if text.lower() is "thanks slarkbot":
+    if text.lower() == "thanks slarkbot":
         update.message.reply_text("You're welcome :)")
