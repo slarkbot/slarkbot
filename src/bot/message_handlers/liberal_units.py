6143b111df994da795a49dfbb4e154f93cb5cc1e
Casper Schuijt <casperschuijt@gmail.com>
2021-03-09 22:20:19 +0100

    Markup and capitalization on message handlers

--- src/bot/message_handlers/liberal_units.py
+++ src/bot/message_handlers/liberal_units.py
@@ -15,5 +15,5 @@ def convert_to_liberal_units(update, context):
         degrees_f = float(re.findall("\x2d?[0-9]*\x2e?[0-9]+", match)[0])
         converted_units = (degrees_f - 32) * 5 / 9
 
-        output = f"{degrees_f} is {converted_units}c in liberal units"
+        output = f"{degrees_f} is {converted_units}°C in liberal units"
         update.message.reply_text(output)
