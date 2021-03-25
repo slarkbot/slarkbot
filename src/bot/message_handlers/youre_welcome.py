72aebb4aa3273df5bab84cc9ef5e71bb0e9bf150
connor <connormullett@gmail.com>
2021-03-25 17:23:49 -0400

    linting pass

--- src/bot/message_handlers/youre_welcome.py
+++ src/bot/message_handlers/youre_welcome.py
@@ -1,10 +1,16 @@
 import re
 
+
 def say_youre_welcome(update, context):
     text = update.message.text
-    text_options = ["thanks slarkbot", "thank you slarkbot", "thanksies slarkbot", "thamks slarkbot"]  
-    text = re.sub(r'[^\w\s]','',text)
-    text = re.sub(' +', ' ',text)
+    text_options = [
+        "thanks slarkbot",
+        "thank you slarkbot",
+        "thanksies slarkbot",
+        "thamks slarkbot",
+    ]
+    text = re.sub(r"[^\w\s]", "", text)
+    text = re.sub(" +", " ", text)
 
     if any(x in text.lower() for x in text_options):
         update.message.reply_text("You're welcome :)")
