f4d350facce88f292c8ad20ca8d29f91498e520e
connor <connormullett@gmail.com>
2021-03-30 22:21:01 -0400

    lowercase seed data for easy lookup

--- scripts/seed.py
+++ scripts/seed.py
@@ -30,15 +30,28 @@ def main():
     aliases = read_json("src/constant_data/aliases.json")
     items = read_json("src/constant_data/items.json")
 
+    all_heroes = []
+    for hero_obj in heroes:
+        all_heroes.append(
+            {
+                "id": hero_obj["id"],
+                "name": hero_obj["name"],
+                "localized_name": hero_obj["localized_name"].lower(),
+                "primary_attr": hero_obj["primary_attr"],
+                "roles": hero_obj["roles"],
+            }
+        )
+
     all_aliases = []
     for alias_obj in aliases:
         for alias in alias_obj["aliases"]:
-            all_aliases.append({"hero_id": alias_obj["id"], "alias": alias})
+            all_aliases.append(
+                {"hero_id": alias_obj["id"], "alias": alias.lower()})
 
     all_items = []
     for item_name, item_data in items.items():
-        display_name = item_data.get("dname") or item_name.replace("_", " ").title()
-        new_item = {"id": item_data["id"], "item_name": display_name}
+        display_name = item_data.get("dname") or item_name.replace("_", " ")
+        new_item = {"id": item_data["id"], "item_name": display_name.lower()}
         all_items.append(new_item)
 
     conn_url = f"postgres://{USER}:{PASSWORD}@localhost:{PORT}/{DATABASE_NAME}"
@@ -58,7 +71,7 @@ def main():
                 %(roles)s
             ) ON CONFLICT DO NOTHING;
             """,
-            heroes,
+            all_heroes,
         )
 
         print(f"{GREEN}SEEDING ALIASES{ENDC}")
--- src/bot/commands/hero_commands.py
+++ src/bot/commands/hero_commands.py
@@ -1,4 +1,5 @@
 from src.bot.services.item_services import get_item_by_id
+from src.bot.services.hero_services import get_hero_by_name
 
 
 def run_suggested_builds_command(update, context):
