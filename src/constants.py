d21790e9e208bd17fee13df48c172102d0a59954
Casper Schuijt <casperschuijt@gmail.com>
2021-04-15 13:08:46 +0200

    fix /help markdown parsing

--- src/constants.py
+++ src/constants.py
@@ -25,9 +25,9 @@ HELP_TEXT = """
     `\/rank <user:optional>` :: Gets a user's current medal\. Defaults to you if no argument is given\. User must be registered for this to work \n
     `\/winrate <user:optional> <hero name>` :: Gets your or someone else's winrate with the given hero\. User must be registered for this to work\n
     `\/profile <user:optional>` :: Get a link to your or someone else's steam profile\n
-    `\/build <hero name or alias>` :: Get recommended items throughout different phases of the game. Example :: \/build <hero name or alias>\n
+    `\/build <hero name or alias>` :: Get recommended items throughout different phases of the game\n
     `\/alias <hero name>` :: Get aliases for a hero\n
-    `\/counters <hero name>` :: Get a list of heroes that counter the given hero. Includes win rates and the percent disadvantage\n
+    `\/counters <hero name>` :: Get a list of heroes that counter the given hero\. Includes win rates and the percent disadvantage\n
     """
 
 WEBSCRAPER_USER_AGENT_HEADER = {
