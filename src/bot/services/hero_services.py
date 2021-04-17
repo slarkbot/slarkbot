d7dd3cddc9c6c3a51337eb43d880438744fc642c
connor <connormullett@gmail.com>
2021-04-17 18:57:54 -0400

    lint pass

--- src/bot/services/hero_services.py
+++ src/bot/services/hero_services.py
@@ -5,8 +5,7 @@ from src.bot.models.hero_alias import HeroAlias
 
 def get_hero_by_name(hero_name):
     session = create_session()
-    output = session.query(Hero).filter(
-        Hero.localized_name.ilike(hero_name)).first()
+    output = session.query(Hero).filter(Hero.localized_name.ilike(hero_name)).first()
     session.close()
     return output
 
@@ -20,15 +19,13 @@ def get_hero_by_id(hero_id):
 
 def get_hero_alias_by_name(hero_alias):
     session = create_session()
-    output = session.query(HeroAlias).filter(
-        HeroAlias.alias.ilike(hero_alias)).first()
+    output = session.query(HeroAlias).filter(HeroAlias.alias.ilike(hero_alias)).first()
     session.close()
     return output
 
 
 def get_hero_aliases_by_hero_id(hero_id):
     session = create_session()
-    output = session.query(HeroAlias).filter(
-        HeroAlias.hero_id == hero_id).all()
+    output = session.query(HeroAlias).filter(HeroAlias.hero_id == hero_id).all()
     session.close()
     return output
--- src/bot/services/user_services.py
+++ src/bot/services/user_services.py
@@ -6,8 +6,7 @@ def lookup_user_by_telegram_handle(telegram_handle):
     telegram_handle = telegram_handle.lower().replace("@", "")
     session = create_session()
     bot_user = (
-        session.query(User).filter(
-            User.telegram_handle == telegram_handle).first()
+        session.query(User).filter(User.telegram_handle == telegram_handle).first()
     )
     session.close()
     return bot_user
@@ -17,7 +16,6 @@ def lookup_user_by_account_id(account_id):
     account_id = int(account_id)
 
     session = create_session()
-    bot_user = session.query(User).filter(
-        User.account_id == account_id).first()
+    bot_user = session.query(User).filter(User.account_id == account_id).first()
     session.close()
     return bot_user
