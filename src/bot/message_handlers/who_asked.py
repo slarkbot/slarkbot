3dc3f1a3285820fd2de433df910f4a13b1214d44
connor <connormullett@gmail.com>
2021-04-26 19:13:44 -0400

    add who_asked

---
+++ src/bot/message_handlers/who_asked.py
@@ -0,0 +1,8 @@
+
+def who_asked(update, context):
+    text = update.message.text
+
+    if text == "!who":
+        update.message.reply_text(
+            "ɴᴏᴡ ᴘʟᴀʏɪɴɢ: Who asked (Feat: Nobody) ───────────⚪️────── ◄◄⠀▐▐⠀►► 𝟸:𝟷𝟾 / 𝟹:𝟻𝟼⠀───○ 🔊"
+        )
