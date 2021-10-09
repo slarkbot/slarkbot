52689a716c34d3515c07cd444bca270c439d3e14
Casper Schuijt <casperschuijt@gmail.com>
2021-10-09 18:39:42 +0200

    updated_at column in users model

--- src/bot/models/user.py
+++ src/bot/models/user.py
@@ -1,3 +1,4 @@
+from sqlalchemy.sql.sqltypes import DateTime
 from . import Base
 from sqlalchemy import Column, String, Integer, BigInteger
 
@@ -6,12 +7,18 @@ class User(Base):
 
     __tablename__ = "bot_users"
 
+    __mapper_args__ = {
+        "order_by":updated_at
+    }
+
     def __init__(self, telegram_handle, account_id, chat_id):
         self.telegram_handle = telegram_handle
         self.account_id = account_id
         self.chat_id = chat_id
+        self.updated_at = updated_at
 
     user_id = Column(Integer, primary_key=True)
     telegram_handle = Column(String)
     account_id = Column(BigInteger)
     chat_id = Column(BigInteger)
+    updated_at = Column(DateTime)
