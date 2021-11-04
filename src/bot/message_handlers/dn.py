871b296b400c04cfd666362919183dc2c25f8d11
connormullett <connormullett@gmail.com>
2021-11-04 16:55:58 -0400

    hotfix 3.0

--- src/bot/message_handlers/dn.py
+++ src/bot/message_handlers/dn.py
@@ -3,7 +3,7 @@ import re
 
 def whats_dn(update, context):
     text = update.message.text
-    search_for = " dn"
+    search_for = "dn"
 
     if search_for in text.lower():
         update.message.reply_text("What's DN? :)")
